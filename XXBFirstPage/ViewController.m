50995310592715b23f25ac7d3b33045953c01ca2
xxb <xxb@xxbdeiMac.local>
2015-07-10 10:31:14 +0800

    Initial Commit

---
+++ XXBFirstPage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XXBFirstPage
+//
+//  Created by xxb on 15/7/10.
+//  Copyright (c) 2015年 xxb. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
